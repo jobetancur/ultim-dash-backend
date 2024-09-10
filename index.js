61ad3337954f6d4974e16991ccb517fe100bbc1b
Alejandro Betancur <99766442+jobetancur@users.noreply.github.com>
2024-09-09 20:47:52 -0500

    add async notification function

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import express from 'express';
 const app = express();
 
 import { initializeApp } from "firebase/app";
-import { doc, getDoc, getFirestore } from "firebase/firestore";
-import { updateDoc } from "firebase/firestore"; 
+import { doc, getDoc, getFirestore, collection, query, where, updateDoc, getDocs } from "firebase/firestore";
 import dotenv from 'dotenv';
+import schedule from 'node-schedule';
+import { createClient } from '@supabase/supabase-js';
+import { v4 as uuidv4 } from 'uuid';
 
 dotenv.config();
 
@@ -20,11 +22,16 @@ const firebaseConfig = {
     measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
-const appFirebase = initializeApp(firebaseConfig);
 
-// Initialize Cloud Firestore and get a reference to the service
+const appFirebase = initializeApp(firebaseConfig);
 const db = getFirestore(appFirebase);
+
+
+// Supabase
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_ANON_KEY
+);
 
 // Middleware para parsear JSON
 app.use(express.json());
@@ -58,6 +65,93 @@ app.post('/clicks', async (req, res) => {
         res.status(404).json({ status: 'error', message: 'Document not found' });
     }
 });
+
+// Función para verificar condiciones de la conversación
+const checkConversations = async () => {
+    console.log('Checking conversations...');
+
+    const { data: conversations, error } = await supabase
+      .from('chat_history')
+      .select('id, agreement, messages, notification_sent')
+      .eq('agreement', false) // Obtener las conversaciones donde el acuerdo es false
+      .eq('notification_sent', false); // Solo las conversaciones sin notificación enviada
+  
+    if (error) {
+      console.error('Error fetching conversations:', error);
+      return;
+    }
+  
+    const now = new Date();
+
+    for (const conversation of conversations) {
+        const lastMessage = conversation.messages[conversation.messages.length - 1];
+        const lastMessageDate = new Date(lastMessage.date);
+        const timeDiff = now - lastMessageDate; 
+
+        // Verifica ambas condiciones: acuerdo es false y más de 3 horas sin interacción
+        if (conversation.agreement === false && timeDiff >= 1 * 60 * 1000 && timeDiff <= 3 * 60 * 1000) {
+          // Genera la notificación si no ha sido creada
+            if (!conversation.notification_sent) {
+                const date = new Date();
+                const options = {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    second: 'numeric',
+                    hour12: true,
+                    timeZone: 'America/Bogota'
+                };
+                const formattedDate = new Intl.DateTimeFormat('es-CO', options).format(date);
+                
+                const notification = {
+                    date: formattedDate,
+                    for:'pruebas@coltefinanciera.com',
+                    from: conversation.client_number,
+                    type: 'new_message',
+                    icon: 'https://firebasestorage.googleapis.com/v0/b/ultim-admin-dashboard.appspot.com/o/avatar%2Fmessage-square-exclamation-svgrepo-com.svg?alt=media&token=c1515841-8574-4dc8-9d51-d6cfa1dd9d34',
+                    id: uuidv4(),
+                    isArchived: false,
+                    isRead: false,
+                    message: 'You have unfinished conversations',
+                    name: 'Chat Alert',
+                };
+
+                createNotification('coltefinanciera', notification);
+
+                // Marca la conversación como notificada
+                await supabase
+                  .from('chat_history')
+                  .update({ notification_sent: true })
+                  .eq('id', conversation.id);
+            }
+        }
+
+        // Si pasan 24 horas sin interacción, se cierra la conversación y se resetea notification_sent a false
+        if (timeDiff >= 24 * 60 * 60 * 1000) {
+            await supabase
+              .from('chat_history')
+              .update({ notification_sent: false })
+              .eq('id', conversation.id);
+        }
+    }
+};
+  
+// Programar la tarea cada minuto
+schedule.scheduleJob('*/1 * * * *', checkConversations);
+
+// Función para crear una notificación en Firebase. Se guardan en la collection organizations. Se debe ubicar la organización por su nombre. Y se debe hacer push del objeto con la notificación al array notifications.
+const createNotification = async (organizationName, notification) => {
+    try {
+        const orgDocRef = doc(db, 'organizations', organizationName);
+        await updateDoc(orgDocRef, {
+            notifications: arrayUnion(notification)
+        });
+    } catch (error) {
+        console.error('Error creating notification:', error);
+    }
+};
 
 // Get prueba
 app.get('/', (req, res) => {
